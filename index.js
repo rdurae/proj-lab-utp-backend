f58d52500a375f0b9b34fd3d791a4bb0fa079842
RodrigoD <rdurae@gmail.com>
2021-07-12 17:27:07 -0500

    expressjs instalado

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-//git config core.autocrlf false       
+//git config core.autocrlf false   
+//iniciar con npm start, definido en package.json
+//instalado npm install express --save    
+   
 console.log("hola mundo");
